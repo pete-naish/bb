f4840132e8ebf2fdafcd683475fad926baf8850b
Pete Naish <pete@nai.sh>
2013-03-25 23:14:20 +0000

    init project with sass, grunt

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Beauty Box Knebworth</title>
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="stylesheet" href="css/main.css">
+		<!--[if lt IE 9]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+
+	</body>
+</html>
