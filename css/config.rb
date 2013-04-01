d2526251ff296b27fdd87cfd8571645ae306d40b
Pete Naish <pete@nai.sh>
2013-04-01 18:49:19 +0100

    removed grunt. moved to codekit. set up basics

---
+++ css/config.rb
@@ -0,0 +1,26 @@
+css_dir = "."
+sass_dir = "."
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+images_dir = "../img"
+javascripts_dir = "../js"
+fonts_dir = "../type"
+
+output_style = :expanded
+environment = :development
+
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass css scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <!doctype html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8">
 		<title>Beauty Box Knebworth</title>
 		<meta name="description" content="">
-		<meta name="author" content="">
-		<link rel="stylesheet" href="css/main.css">
+		<meta name="keywords" content="">
+		<link rel="stylesheet" href="css/screen.css" media="screen">
 		<!--[if lt IE 9]>
 			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+			<link rel="stylesheet" href="css/ie.css">
 		<![endif]-->
+		<link rel="stylesheet" href="css/print.css" media="screen">
 	</head>
 	<body>
 
