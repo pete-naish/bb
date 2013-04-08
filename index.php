725c6929a5c56034df2b500b490fff8ed543db06
Pete Naish <pete.naish@gmail.com>
2013-04-08 22:01:06 +0100

    added about section, svg logo, content:after flourishes

--- index.php
+++ index.php
@@ -17,14 +17,17 @@
 			<div class="container">
 				<h1 class="visuallyhidden">Beauty Box Knebworth</h1>
 				<a href="index.php" class="logo" title="Beauty Box Knebworth home page">
-					<img src="img/logo.png" alt="Beauty Box Knebworth logo">
+					<!--[if lte IE 8]>
+						<img src="img/logo.png" alt="Beauty Box Knebworth logo">
+					<![endif]-->
 				</a>
+				
 				<h5>For appointments call</h5>
 				<a href="tel:+441438812804" class="tel">01438 812 804</a>
 				<nav>
 					<ul>
-						<li><a href="#">Treatments</a><i>{</i></li>
-						<li><a href="#">About us</a><i>{</i></li>
+						<li><a href="#">Treatments</a><i></i></li>
+						<li><a href="#">About us</a><i></i></li>
 						<li><a href="#">Contact us</a></li>
 					</ul>
 				</nav>
@@ -53,9 +56,9 @@
 		<section>
 			<div class="container">
 				<div class="treatments">
-					<i>{</i>
+					<i></i>
 					<h3>Treatments</h3>
-					<div class="treatment">
+					<div class="treatmentIntro">
 						<a href="#">
 							<img src="img/nails.jpg" alt="">
 						</a>
@@ -63,7 +66,7 @@
 						<p>Let us look after your hands and feet, so they're perfect for every occasion</p>
 						<a href="#" class="cta">Treatments &amp; prices</a>
 					</div>
-					<div class="treatment">
+					<div class="treatmentIntro">
 						<a href="#">
 							<img src="img/nails.jpg" alt="">
 						</a>
@@ -71,7 +74,7 @@
 						<p>Let us look after your hands and feet, so they're perfect for every occasion</p>
 						<a href="#" class="cta">Treatments &amp; prices</a>
 					</div>
-					<div class="treatment">
+					<div class="treatmentIntro">
 						<a href="#">
 							<img src="img/nails.jpg" alt="">
 						</a>
@@ -79,13 +82,93 @@
 						<p>Let us look after your hands and feet, so they're perfect for every occasion</p>
 						<a href="#" class="cta">Treatments &amp; prices</a>
 					</div>
-					<div class="treatment">
+					<div class="treatmentIntro">
 						<a href="#">
 							<img src="img/nails.jpg" alt="">
 						</a>
 						<h4>Hands &amp; feet</h4>
 						<p>Let us look after your hands and feet, so they're perfect for every occasion</p>
 						<a href="#" class="cta">Treatments &amp; prices</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="container">
+				<div class="about">
+					<i></i>
+					<h3>About us</h3>
+					<div class="row">
+						<div class="colThird">
+							<h4>Reservations</h4>
+							<p>Upon booking we require all contact details and you must make us aware of any allergies or illnessive you may have.</p>
+						</div>
+						<div class="colThird">
+							<h4>Opening hours</h4>
+							<table class="hours" border="0">
+								<tr>
+									<th>Monday</th>
+									<td>Closed</td>
+									<td class="dash">&ndash;</td>
+									<td>Closed</td>
+							</tr>
+							<tr>
+								<th>Tuesday</th>
+								<td>10:00</td>
+								<td class="dash">&ndash;</td>
+								<td>20:00</td>
+							</tr>
+							<tr>
+								<th>Wednesday</th>
+								<td>10:00</td>
+								<td class="dash">&ndash;</td>
+								<td>20:00</td>
+							</tr>
+							<tr>
+								<th>Thursday</th>
+								<td>10:00</td>
+								<td class="dash">&ndash;</td>
+								<td>20:00</td>
+							</tr>
+							<tr>
+								<th>Friday</th>
+								<td>10:00</td>
+								<td class="dash">&ndash;</td>
+								<td>18:00</td>
+							</tr>
+							<tr>
+								<th>Saturday</th>
+								<td>10:00</td>
+								<td class="dash">&ndash;</td>
+								<td>16:00</td>
+							</tr>
+							<tr>
+								<th>Sunday</th>
+								<td>Closed</td>
+								<td class="dash">&ndash;</td>
+								<td>Closed</td>
+							</tr>
+							</table>
+						</div>
+						<div class="colThird">
+							<h4>Cancellation policy</h4>
+							<p>If you need to cancel your appointment, please provide us with at least 24 hours notice in order to avoid being charged a minimum fee of 50%. Missed appointments will be charged at the full treatment cost.</p>
+						</div>
+					</div>
+					<i class="small"></i>
+					<div class="row">
+						<div class="colThird">
+							<h4>Gift vouchers</h4>
+							<p>The perfect gift&mdash;our vouchers are redeemable against any treatments and products at the salon, and are valid for 6 months from the issue date. Gift vouchers are non-transferrable and non-refundable.</p>
+						</div>
+						<div class="colThird">
+							<h4>Children</h4>
+							<p>For the benefit of all clients, and to comply with our health and safety regulations, children cannot accompany parents into the treatment room. We do not have the facility to supervise children whilst parents are being treated. Clients must be over the age of 18; however, some treatments can be applied to those between 14 and 18 years, provided a parent or guardian is present to sign a consent form.</p>
+						</div>
+						<div class="colThird">
+							<h4>Men's treatments</h4>
+							<p>Most of our treatments are suitable for men. Please bring suitable shorts or swimwear for body treatments.</p>
+						</div>
 					</div>
 				</div>
 			</div>
