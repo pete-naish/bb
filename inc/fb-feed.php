cfc6bfd749691d9f22d9effab476c698dfeadba9
Pete Naish <pete.naish@gmail.com>
2015-08-25 10:58:33 +0100

    updating includes

--- inc/fb-feed.php
+++ inc/fb-feed.php
@@ -39,14 +39,14 @@ foreach($pagefeed['data'] as $post) {
  
         // open up an fb-update div
         echo "<div class=\"status-article\">";
- 		echo $feedpic;
- 		echo "<div class=\"feed-text\"><p>";
+        echo $feedpic;
+        echo "<div class=\"feed-text\"><p>";
             // post the time
  
             // check if post type is a status
             if ($post['type'] == 'status' && $post['story'] == '') {
 
-            	$statuslink .= str_replace('187845997900472_', '', $post['id']);
+                $statuslink .= str_replace('187845997900472_', '', $post['id']);
 
                 echo "<span class=\"feed-time\"><a href=\"" . $statuslink . "\">" . pretty_relative_time(strtotime($post['created_time'])) . "</a></span>";
                 // echo "<span class=\"feed-time\">" . pretty_relative_time(strtotime($post['created_time'])) . "</span>";
@@ -89,26 +89,26 @@ foreach($pagefeed['data'] as $post) {
 } // end the foreach statement
  
 function pretty_relative_time($time) {
-	if ($time !== intval($time)) { $time = strtotime($time); }
-	$d = time() - $time;
-	if ($time < strtotime(date('Y-m-d 00:00:00')) - 60*60*24*3) {
-	$format = 'F j';
-	if (date('Y') !== date('Y', $time)) {
-	$format .= ", Y";
-	}
-	return date($format, $time);
-	}
-	if ($d >= 60*60*24) {
-	$day = 'Yesterday';
-	if (date('l', time() - 60*60*24) !== date('l', $time)) { $day = date('l', $time); }
-	return $day . " at " . date('g:ia', $time);
-	}
-	if ($d >= 60*60*2) { return intval($d / (60*60)) . " hours ago"; }
-	if ($d >= 60*60) { return "about an hour ago"; }
-	if ($d >= 60*2) { return intval($d / 60) . " minutes ago"; }
-	if ($d >= 60) { return "about a minute ago"; }
-	if ($d >= 2) { return intval($d) . " seconds ago"; }
-	return "a few seconds ago";
+    if ($time !== intval($time)) { $time = strtotime($time); }
+    $d = time() - $time;
+    if ($time < strtotime(date('Y-m-d 00:00:00')) - 60*60*24*3) {
+    $format = 'F j';
+    if (date('Y') !== date('Y', $time)) {
+    $format .= ", Y";
+    }
+    return date($format, $time);
+    }
+    if ($d >= 60*60*24) {
+    $day = 'Yesterday';
+    if (date('l', time() - 60*60*24) !== date('l', $time)) { $day = date('l', $time); }
+    return $day . " at " . date('g:ia', $time);
+    }
+    if ($d >= 60*60*2) { return intval($d / (60*60)) . " hours ago"; }
+    if ($d >= 60*60) { return "about an hour ago"; }
+    if ($d >= 60*2) { return intval($d / 60) . " minutes ago"; }
+    if ($d >= 60) { return "about a minute ago"; }
+    if ($d >= 2) { return intval($d) . " seconds ago"; }
+    return "a few seconds ago";
 }
 
 // echo "</div>";
